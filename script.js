bf7125f62095692db09479a9cb782187ba2b06d2
W1lsonCodes <48604034+W1lsonCodes@users.noreply.github.com>
2020-08-10 18:20:10 +0100

    completed 'showYouWon'

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
  * DONE: Console whether the guess is too high, too low, or is correct inside playGame function
  * DONE: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
  * DONE: Complete the showYouWon, showNumberAbove, showNumberBelow
- * TODO: Use the showYouWon... functions within displayResult to display the correct dialog
+ * DONE: Use the showYouWon... functions within displayResult to display the correct dialog
  * TODO: Save the guess history in a variable called guess
  * TODO: Display the guess history using displayHistory() function
  * TODO: Use the initGame() function to restart the game
@@ -15,7 +15,6 @@
 
 // Variable for store the correct random number  
 let correctNumber = getRandomNumber();
-
 
 window.onload = function() {
     document.getElementById("number-submit").addEventListener("click", playGame);
@@ -37,12 +36,13 @@ function playGame(){
  */
 function displayResult(numberGuess){
   if(numberGuess > correctNumber){
-    console.log("too high");
+    showNumberAbove();
   } else if(numberGuess < correctNumber){
-    console.log("too low");
+    showNumberBelow();
   } else {
-    console.log("is correct");
+    showYouWon();
   }
+  return getDialog();
 }
 
 /**
