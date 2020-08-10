8efcee2730b8c29edaec31c6708f6b81bd0afa35
W1lsonCodes <48604034+W1lsonCodes@users.noreply.github.com>
2020-08-10 16:56:35 +0100

    added function displayResult

--- script.js
+++ script.js
@@ -28,7 +28,14 @@ window.onload = function() {
 function playGame(){
   // *CODE GOES BELOW HERE *
   let numberGuess = document.getElementById('number-guess').value;
-  console.log(correctNumber);
+  displayResult(numberGuess);
+}
+
+/**
+ * Show the result for if the guess it too high, too low, or correct
+ * HINT: Use if, else if, else statement 
+ */
+function displayResult(numberGuess){
   if(numberGuess > correctNumber){
     console.log("too high");
   } else if(numberGuess < correctNumber){
@@ -37,13 +44,6 @@ function playGame(){
     console.log("is correct");
   }
 }
-
-/**
- * Show the result for if the guess it too high, too low, or correct
- * HINT: Use if, else if, else statement 
- */
-// *CODE GOES BELOW HERE *
-
 
 /**
  * Initialize a new game by resetting all values and content on the page
